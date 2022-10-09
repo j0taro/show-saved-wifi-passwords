394677f503f8e96faecf318de5b59b694d501cba
jotaro <93826447+j0taro@users.noreply.github.com>
2022-10-09 21:00:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import subprocess
 from colorama import Fore
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+print("{:<30}      | {:<}".format(f"{Fore.RED} Wi-Fi Name", f"{Fore.RED}Password"))
+print(f'{Fore.BLUE}'+f"══════════════════════════════════════════{Fore.RESET}")
 for i in profiles:
-    print("{:<30}     | {:<}".format(f"{Fore.RED}Wi-Fi Name", f"{Fore.RED}Password"))
-    print(f'{Fore.BLUE}'+f"══════════════════════════════════════════{Fore.RESET}")
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
     results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
     try:
-        print (f'{Fore.GREEN}'+"{:<30}| {:<}".format(i, results[0]))
+        print (Fore.GREEN,"{:<30}{f}| {:<}".format(i,f'{Fore.RED}' +results[0],f=Fore.BLUE))
     except IndexError:
-        print (f'{Fore.GREEN}'+"{:<30}| {:<}]".format(i, ""))
+        print (Fore.GREEN,"{:<30}{f}| {:<}]".format(i,f'{Fore.RED}'+ "",f=Fore.BLUE))
 input("")
