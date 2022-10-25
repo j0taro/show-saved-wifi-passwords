9e1bfda5f6d407d800ed687af056d057c2b46fd6
timmy <112690978+timmywag2@users.noreply.github.com>
2022-10-25 07:24:23 +0200

    cleaned
    colorama is bad fr
    and formatting is better nwo ezzzzzz

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import subprocess
-from colorama import Fore
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
-print("{:<30}      | {:<}".format(f"{Fore.RED} Wi-Fi Name", f"{Fore.RED}Password"))
-print(f'{Fore.BLUE}'+f"══════════════════════════════════════════{Fore.RESET}")
+print("{:<30}| {:<}".format("Wi-Fi Name", "Password"))
+print("══════════════════════════════════════════")
 for i in profiles:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
     results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
     try:
-        print (Fore.GREEN,"{:<30}{f}| {:<}".format(i,f'{Fore.RED}' +results[0],f=Fore.BLUE))
+        print("{:<30}| {:<}".format(i,results[0]))
     except IndexError:
-        print (Fore.GREEN,"{:<30}{f}| {:<}]".format(i,f'{Fore.RED}'+ "",f=Fore.BLUE))
-input("")
+        print("{:<30}| ".format(i))
